111afe787369091ebae28b22c13f3617b832076d
Nathan Milot <nathan.c.milot@gmail.com>
2017-02-07 14:49:30 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-// A $(document).ready() block
-$(document).ready(function() {
-
-});
-
 (function($){
   $.fn.extend({
     rotaterator: function(options) {
@@ -47,12 +42,12 @@ $(document).ready(function() {
   }
 });
 
-$(document).resize(fuction(){
-                   $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:1000});
+$(document).ready(function() {
+  $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:1000});
   if($(window).height() < $(window).width()){
     $('.resume_frame').css('height', $(window).height()*.7);
   }else{
     $('.resume_frame').css('height', $(window).width()*.7);
   }
-});
+})(jQuery);
                    
